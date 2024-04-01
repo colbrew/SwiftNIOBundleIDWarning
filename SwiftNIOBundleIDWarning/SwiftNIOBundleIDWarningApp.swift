ec8102cc30732f9ab9ca7e01b8a7a65608aa5c8b
Daniel Colman <danRcolman@gmail.com>
2024-04-01 11:45:42 -0400

    Initial Commit

---
+++ SwiftNIOBundleIDWarning/SwiftNIOBundleIDWarningApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftNIOBundleIDWarningApp.swift
+//  SwiftNIOBundleIDWarning
+//
+//  Created by Daniel Colman on 4/1/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftNIOBundleIDWarningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
