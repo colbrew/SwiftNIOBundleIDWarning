ed2793fbdf3904342714903befd852757cdfd414
Daniel Colman <danRcolman@gmail.com>
2024-04-01 14:21:35 -0400

    bundle id warning example

---
+++ FrameworkWithPackages/FrameworkWithPackages.h
@@ -0,0 +1,18 @@
+//
+//  FrameworkWithPackages.h
+//  FrameworkWithPackages
+//
+//  Created by Daniel Colman on 4/1/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FrameworkWithPackages.
+FOUNDATION_EXPORT double FrameworkWithPackagesVersionNumber;
+
+//! Project version string for FrameworkWithPackages.
+FOUNDATION_EXPORT const unsigned char FrameworkWithPackagesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FrameworkWithPackages/PublicHeader.h>
+
+
